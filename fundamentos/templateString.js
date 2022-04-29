3bcdf3834d52c4a30e120bc2a7df0d00657b425f
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-04-29 17:36:13 -0400

    update

---
+++ fundamentos/templateString.js
@@ -0,0 +1,9 @@
+const nome = "Rebeca";
+const concatenacao = "Olá " + nome + "!";
+const template = `Olá ${nome}!`;
+
+console.log(concatenacao, template);
+
+const up = (s) => s.toUpperCase();
+
+console.log(`Ei... ${up("cuidado")}!`);
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,16 @@
+var a = 1;
+let b = 2;
+
+var a = 30;
+b = 40;
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
+
+const c = 3;
+
+console.log(c);
