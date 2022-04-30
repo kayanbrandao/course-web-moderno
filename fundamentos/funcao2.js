d627916ac47e8d519295fe3998aea48193eddbae
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-04-30 18:22:53 -0400

    update

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,13 @@
+// armazenando uma função em uma variavel
+
+const imprimirSoma = function (a, b) {
+	console.log(a + b);
+};
+
+imprimirSoma(2, 3);
+
+// armazenando uma função arrow em uma variavel
+
+const soma = (a, b) => a + b;
+
+console.log(soma(1, 3));
