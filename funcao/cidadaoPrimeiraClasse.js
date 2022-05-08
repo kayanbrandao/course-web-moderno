ebbbe67b354e38fce5b58625089a6166ab600364
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-05-08 12:15:12 -0400

    update

--- funcao/cidadaoPrimeiraClasse.js
+++ funcao/cidadaoPrimeiraClasse.js
@@ -15,3 +15,32 @@ const array = [
 	fun1,
 	fun2,
 ];
+
+console.log(array[0](2, 3));
+
+// Armazenar em um atributo de objeto
+const obj = {};
+
+obj.falar = function () {
+	return "Opa";
+};
+console.log(obj.falar());
+
+// Passar uma função como parm
+function run(fun) {
+	fun();
+}
+run(function () {
+	console.log("Executando...");
+});
+
+// Uma função pode retornar/conter uma função
+function soma(a, b) {
+	return function (c) {
+		console.log(a + b + c);
+	};
+}
+
+soma(2, 3)(4);
+const cincoMais = soma(2, 3);
+cincoMais(4);
