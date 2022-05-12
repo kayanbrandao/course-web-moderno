2ae3a7ac2ef2e97fcc7727bf62dbd45af69f5d21
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-05-12 18:15:59 -0400

    update

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+// Código não executável.
+
+// Procedural
+
+processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+	valor1,
+	valor2,
+	valor3,
+	processamento() {
+		//...
+	},
+};
+
+objeto.processamento(); // Foco passou a ser objeto
+
+// Principios importantes
+// 1. abstração;
+// 2. encapsulamento;
+// 3. herança (prototype);
+// 4. polimorfismo;
