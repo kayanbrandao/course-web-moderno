28975697681fb48c52cb1b520074a5b203a4a588
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-05-14 12:37:28 -0400

    update

---
+++ objeto/jsonVsObj.js
@@ -0,0 +1,16 @@
+const obj = {
+	a: 1,
+	b: 2,
+	c: 3,
+	soma() {
+		return a + b + c;
+	},
+};
+
+console.log(JSON.stringify(obj));
+// console.log(JSON.parse("{ a:1, b:2, c:3 }"));
+// console.log(JSON.parse("{ 'a':1, 'b':2, 'c':3 }"));
+console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }'));
+console.log(
+	JSON.parse('{ "a": 1.17, "b": "string", "c": false, "d": {}, "e": []  }')
+);
